f17fc6c4408d07240eff969cbfb81437c509db47
MATSUDA Takashi <matsuda@hallab.co.jp>
2020-04-19 14:08:37 +0900

    fix: remove unnecessary introduced variable

--- src/util/pathRewrite.ts
+++ src/util/pathRewrite.ts
@@ -1,29 +1,32 @@
 import { NeovimClient as Neovim } from '@chemzqm/neovim'
 
-type RewriteRule = {
-  pattern: string;
-  replace: string;
+type RewriteRules = {
+  [prefix: string]: string
 }
 
-const DefaultRewriteRules = [{ pattern: '^/([A-Za-z])/', replace: '$1:' }]
+const DefaultPathPrefixes = { '/([A-Z])/': '$1:' }
 
 let _pathRewrite: (path: string) => string | null = null
 
 export async function createPathRewrite(nvim: Neovim): Promise<(p: string) => string> {
-  let rawRules: RewriteRule[] = []
+  let rawRules: RewriteRules = null;
   if (await nvim.eval('has("win32unix")')) {
-    rawRules = await nvim.eval('get(g:, "coc_cygpath_rewrite_rules", v:null)') as RewriteRule[]
+    rawRules = await nvim.eval('get(g:, "coc_cygqwin_path_prefixes", v:null)') as RewriteRules
     if (rawRules == null) {
-        rawRules = DefaultRewriteRules
+        rawRules = DefaultPathPrefixes
     }
   }
-  const rules = rawRules
-    ?.filter(({pattern, replace}) => pattern && replace)
-    .map(({pattern, replace}) => ({ regexp: new RegExp(pattern), replace }))
+  let rules: { regexp: RegExp; replace: string }[] = []
+  if (rawRules && typeof rawRules === "object") {
+    rules = Object.keys(rawRules as RewriteRules)
+      .filter(k => k && rawRules[k])
+      .map(k => ({ regexp: new RegExp('^' + k, 'gi'), replace: rawRules[k] }))
+  }
   if (rules && rules.length > 0) {
     const rewrite = (p: string) => rules.reduce((prev, { regexp, replace }) => prev.replace(regexp, replace), p)
     return (p) => {
       if (p) {
+        p = p.replace(/\\/g, '/')
         if (p.indexOf(',') >= 0) {
           return p.split(',').map(rewrite).join(',')
         } else {
